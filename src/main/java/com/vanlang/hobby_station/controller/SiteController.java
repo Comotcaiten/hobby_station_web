7514b4a38332543c5e051b14460fff4829347ebd
Comotcaiten <pikachu123450vn@gmail.com>
2024-07-18 13:26:24 +0700

    backup truoc khi lam single page application

--- src/main/java/com/vanlang/hobby_station/controller/SiteController.java
+++ src/main/java/com/vanlang/hobby_station/controller/SiteController.java
@@ -3,12 +3,17 @@ package com.vanlang.hobby_station.controller;
 import com.vanlang.hobby_station.model.Product;
 import com.vanlang.hobby_station.service.CategoryService;
 import com.vanlang.hobby_station.service.ProductService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping
@@ -45,5 +50,20 @@ public class SiteController {
         model.addAttribute("content", "/site/detail-product");
         return "layout";
     }
+
+    @GetMapping("/search")
+    public String getMethodName(@RequestParam(value = "name", required = false) String name, Model model) {
+        List<Product> products;
+        if (name == null || name.trim().isEmpty()) {
+            products = productService.getAllProducts();
+        } else {
+            products = productService.searchProductsByName(name);
+        }
+
+        model.addAttribute("products", products);
+        model.addAttribute("content", "/site/search");
+        return "layout";
+    }
+    
     
 }
